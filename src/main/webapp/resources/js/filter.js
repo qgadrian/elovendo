1c591239bfe6cc4903e36b0d0153e2fe96186da4
adrian <qgadrian@gmail.com>
2014-10-22 20:27:36 +0200

    71: Search by radius in item list show lat and lng as 0
    Task-Url: http://github.com/qgadrian/telocompro/issues/issue/71

--- src/main/webapp/resources/js/filter.js
+++ src/main/webapp/resources/js/filter.js
@@ -6,24 +6,20 @@ function setRad(dis) {
 	var ma = getURLParameter('max');
 	
 	if (mi != null) {
-		console.log("here we are");
-//		var o = document.createElement('input');
-//		o.value = mi;
-//		o.innerHTML = "min";
-//		o.type = "hidden";
-//		$('#locationForm').append(o);
 		$('<input>').attr({
-		    type: 'hidden',
-		    value: mi,
-		    name: 'min'
-		}).appendTo('#locationForm');
+			type : 'hidden',
+			value : mi,
+			name : 'min',
+			id: 'min'
+		}).appendTo('#locform');
 	}
 	if (ma != null) {
-		var o = document.createElement('input');
-		o.value = ma;
-		o.innerHTML = "max";
-		o.type = "hidden";
-		$('#locationForm').append(o);
+		$('<input>').attr({
+			type : 'hidden',
+			value : ma,
+			name : 'max',
+			id: 'max'
+		}).appendTo('#locform');
 	}
 	
 	$('#locform').submit();
@@ -109,19 +105,16 @@ function dieRmv(val) {
 //}
 
 function removeParam(key, sourceURL) {
-    var rtn = sourceURL.split("?")[0],
-        param,
-        params_arr = [],
-        queryString = (sourceURL.indexOf("?") !== -1) ? sourceURL.split("?")[1] : "";
-    if (queryString !== "") {
-        params_arr = queryString.split("&");
-        for (var i = params_arr.length - 1; i >= 0; i -= 1) {
-            param = params_arr[i].split("=")[0];
-            if (param === key) {
-                params_arr.splice(i, 1);
-            }
-        }
-        rtn = rtn + "?" + params_arr.join("&");
-    }
-    return rtn;
+	var rtn = sourceURL.split("?")[0], param, params_arr = [], queryString = (sourceURL.indexOf("?") !== -1) ? sourceURL.split("?")[1] : "";
+	if (queryString !== "") {
+		params_arr = queryString.split("&");
+		for (var i = params_arr.length - 1; i >= 0; i -= 1) {
+			param = params_arr[i].split("=")[0];
+			if (param === key) {
+				params_arr.splice(i, 1);
+			}
+		}
+		rtn = rtn + "?" + params_arr.join("&");
+	}
+	return rtn;
 }
--- src/main/webapp/resources/js/geo.js
+++ src/main/webapp/resources/js/geo.js
@@ -1,118 +1,117 @@
 var placeSearch, autocomplete;
-		function initAutoComplete() {
-		  // Create the autocomplete object, restricting the search
-		  // to geographical location types.
-		  autocomplete = new google.maps.places.Autocomplete(
-		      /** @type {HTMLInputElement} */(document.getElementById('autocomplete')),
-		      { types: ['geocode'] });
-		  // When the user selects an address from the dropdown,
-		  // populate the address fields in the form.
-		  google.maps.event.addListener(autocomplete, 'place_changed', function() {
-		    geoAddress();
-		  });
-		}
-		
-		function geoAddress() {
-			var place = autocomplete.getPlace();
-// 			console.log("result: " + place.geometry.location.lat());
-// 			console.log("result: " + place.geometry.location.lng());
-			document.getElementById('lat').value = place.geometry.location.lat();
-			document.getElementById('lng').value = place.geometry.location.lng();
-			if (map != null)
-				addMarker(place.geometry.location.lat(), place.geometry.location.lng());
-		}
-		
-		// Add a marker to the map and push to the array.
-//TODO: search first makes no marker appear in map
-			  function addMarker(lat, lng) {
-				  var location = new google.maps.LatLng(lat, lng, false); 
-				  if (marker == null) {
-				    marker = new google.maps.Marker({
-				      position: location,
-				      map: map
-				    });
-			    } else marker.setPosition(location);
-			    
-			    map.setCenter(location);
-// 			    markers.push(marker);
-			  }
+function initAutoComplete() {
+	// Create the autocomplete object, restricting the search
+	// to geographical location types.
+	autocomplete = new google.maps.places.Autocomplete(
+	/** @type {HTMLInputElement} */
+	(document.getElementById('autocomplete')), {
+		types : [ 'geocode' ]
+	});
+	// When the user selects an address from the dropdown,
+	// populate the address fields in the form.
+	google.maps.event.addListener(autocomplete, 'place_changed', function() {
+		geoAddress();
+	});
+}
 
+function geoAddress() {
+	var place = autocomplete.getPlace();
+	document.getElementById('lat').value = place.geometry.location.lat();
+	document.getElementById('lng').value = place.geometry.location.lng();
+	if (map != null)
+		addMarker(place.geometry.location.lat(), place.geometry.location.lng());
+}
 
-		var loc;
-		var marker;
-		var map;
-		var la = -34.397;
-		var lo = 150.644;
-		
-		function initialize() {
-			getLocation();
-			var mapOptions = {
-				center : new google.maps.LatLng(la, lo),
-				zoom : 1,
-				maxZoom: 18,
-				streetViewControl: false,
-				mapTypeId : google.maps.MapTypeId.ROADMAP
-			};
-			map = new google.maps.Map(
-				document.getElementById("map_canvas"), mapOptions);
+// Add a marker to the map and push to the array.
+// TODO: search first makes no marker appear in map
+function addMarker(lat, lng) {
+	var location = new google.maps.LatLng(lat, lng, false);
+	if (marker == null) {
+		marker = new google.maps.Marker({
+			position : location,
+			map : map
+		});
+	} else
+		marker.setPosition(location);
+	map.setCenter(location);
+	// markers.push(marker);
+}
 
-			// This event listener will call addMarker() when the map is clicked.
-			google.maps.event.addListener(map, 'click', function(event) {
-				marker.setMap(null); // clear marker in map
-				addMarker(event.latLng);
-				document.getElementById("lat").value = event.latLng.lat();
-				document.getElementById("lng").value = event.latLng.lng();
-				document.getElementById('createButton').disabled = false;
-			});
-			
-			// Add a marker to the map and push to the array.
-			  function addMarker(location) {
-			    marker = new google.maps.Marker({
-			      position: location,
-			      map: map
-			    });
-// 			    markers.push(marker);
-			  }
-			
-			// To add the marker to the map, use the 'map' property
-// 			var marker = new google.maps.Marker({
-// 				position: coorLang,
-// 				map: map,
-// 				title:"Localización"
-// 			});
-			marker = new google.maps.Marker();
+var loc;
+var marker;
+var map;
+var la = -34.397;
+var lo = 150.644;
+
+function initialize() {
+	getLocation();
+	var mapOptions = {
+		center : new google.maps.LatLng(la, lo),
+		zoom : 1,
+		maxZoom : 18,
+		streetViewControl : false,
+		mapTypeId : google.maps.MapTypeId.ROADMAP
+	};
+	map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+
+	// This event listener will call addMarker() when the map is clicked.
+	google.maps.event.addListener(map, 'click', function(event) {
+		marker.setMap(null); // clear marker in map
+		addMarker(event.latLng);
+		document.getElementById("lat").value = event.latLng.lat();
+		document.getElementById("lng").value = event.latLng.lng();
+		document.getElementById('createButton').disabled = false;
+	});
+
+	// Add a marker to the map and push to the array.
+	function addMarker(location) {
+		marker = new google.maps.Marker({
+			position : location,
+			map : map
+		});
+		// markers.push(marker);
+	}
+
+	// To add the marker to the map, use the 'map' property
+	// var marker = new google.maps.Marker({
+	// position: coorLang,
+	// map: map,
+	// title:"Localización"
+	// });
+	marker = new google.maps.Marker();
+}
+
+function getLocation() {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(getPosition);
+	}
+}
+
+function getPosition(position) {
+	loc = new google.maps.LatLng(position.coords.latitude,
+			position.coords.longitude);
+	map.setCenter(loc);
+	map.setZoom(13);
+}
+
+function initLocation() {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(getInitPosition);
+	}
+}
+
+function getInitPosition(position) {
+	var lat = document.getElementById("lat").value = position.coords.latitude;
+	var lng = document.getElementById("lng").value = position.coords.longitude;
+
+	// initialize geosearch in index
+	var arr = [], l = document.links;
+	for (var i = 0; i < l.length; i++) {
+		// console.log(l[i].href);
+		var ela = l[i].href;
+		if (ela.indexOf("bazaar/category") > -1) {
+			l[i].href = ela + "?lat=" + lat + "&lng=" + lng;
 		}
-		
-		function getLocation() {
-			if (navigator.geolocation) {
-				navigator.geolocation.getCurrentPosition(getPosition);
-			}
-		}
-		
-		function getPosition(position) {
-			loc = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-			map.setCenter(loc);
-			map.setZoom(13);
-		}
-		
-		function initLocation() {
-			if (navigator.geolocation) {
-				navigator.geolocation.getCurrentPosition(getInitPosition);
-			}
-		}
-		
-		function getInitPosition(position) {
-			var lat = document.getElementById("lat").value = position.coords.latitude;
-			var lng = document.getElementById("lng").value = position.coords.longitude;
-			
-			// initialize geosearch in index
-			var arr = [], l = document.links;
-			for(var i=0; i<l.length; i++) {
-//			  console.log(l[i].href);
-			  var ela = l[i].href;
-			  if (ela.indexOf("bazaar/category") > -1) {
-				  l[i].href = ela + "?lat=" + lat + "&lng=" + lng;
-	 			}
-			}
-			
-		}
+	}
+
+}
